0f35027d1977a3ad87b247e8389b2a903b389e47
ztf <2232050718@qq.com>
2020-03-26 22:28:08 +0800

    2020-3-26-添加QQ登录功能

--- app/Http/Controllers/CommonControllers/QQLogin.php
+++ app/Http/Controllers/CommonControllers/QQLogin.php
@@ -40,14 +40,18 @@ class QQLogin extends Controller
             exit;
         }
         $access_token = $access_token_info['access_token'];
-        $get_openid_url = $qq_login_fg['openid_url'] . $access_token;
+        $get_openid_url = $qq_login_fg['openid_url'] . 'dsd';
         $openid_data = getHttpResponseGET($get_openid_url);
         Log::info('openid' . json_encode($openid_data));
         if (strpos($openid_data, "callback") === false) {
-            dealQQErrorMessage($openid_data, '获取openid失败');
+            echo '获取openid异常，稍后重试！';
             exit;
         }
         $openid_data = dealQQData($openid_data);
+        if (isset($openid_data['error'])) {
+            dealQQErrorMessage($openid_data, '获取openid失败');
+            exit;
+        }
         if (empty($openid_data['openid'])) {
             echo '获取的openid信息为空，请稍后重试';
             exit;

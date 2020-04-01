2ee650eb13dddbc8c0d5aab46412f7ba35f2f56e
zhangtengfei03 <zhangtengfei03@baidu.com>
2020-04-01 12:26:17 +0800

    更改百度登录配置变量名字

--- app/Http/Controllers/CommonControllers/BaiDuLogin.php
+++ app/Http/Controllers/CommonControllers/BaiDuLogin.php
@@ -17,20 +17,20 @@ class BaiDuLogin extends Controller
             echo '没有获取到code';
             exit;
         }
-        $baidu_login_fg = config('baidu');
+        $bai_du_login_cg = config('baidu');
         $param = array(
-            'grant_type' => $baidu_login_fg['grant_type'],
+            'grant_type' => $bai_du_login_cg['grant_type'],
             'code' => $request->code,
-            'client_id' => $baidu_login_fg['client_id'],
-            'client_secret' => $baidu_login_fg['client_secret'],
-            'redirect_uri' => $baidu_login_fg['redirect_uri'],
+            'client_id' => $bai_du_login_cg['client_id'],
+            'client_secret' => $bai_du_login_cg['client_secret'],
+            'redirect_uri' => $bai_du_login_cg['redirect_uri'],
         );
-        $url = $baidu_login_fg['access_token_url'];
+        $url = $bai_du_login_cg['access_token_url'];
         $token = json_decode(getHttpResponsePOST($url, $param), true);
         $access_token = $token['access_token'];
         Log::info('ccccc' . $access_token);
         if (!empty($access_token)) {
-            $url = $baidu_login_fg['user_info_url'] . $access_token;
+            $url = $bai_du_login_cg['user_info_url'] . $access_token;
             $info = getHttpResponseGET($url);
             Log::info($info);
         }
